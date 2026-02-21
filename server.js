022fb20e0bf4f1f02310aa2051fe12b5cf8f5d64
Jose Luis Llorens Climent <praderapedro@gmail.com>
2026-02-21 13:01:22 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ app.get("/api/orders", async (req, res) => {
 if (process.env.NODE_ENV === "production" || process.env.RENDER) {
   app.use(express.static(path.join(__dirname, "dist")));
 
-  app.get("/*", (req, res) => {
+  app.get("/(.*)", (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
   });
 }
